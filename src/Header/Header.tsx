b65137c226f7ddcdcb977b718d23a71f3abc75a6
Ajab011 <ajabflexwala@gmail.com>
2025-04-21 16:58:58 +0530

    Setting up the project

---
+++ src/Header/Header.tsx
@@ -0,0 +1,11 @@
+const Header=()=>{
+    return<div className="w-full bg-black px-6 text-white 
+    h-28 flex justify-between items-center">
+         <div>
+            ijobs
+         </div>
+         <div>links</div>
+         <div>profile</div>
+    </div>
+}
+export default Header;
